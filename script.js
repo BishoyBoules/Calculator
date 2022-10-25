a17ef20b12ee4dd0953e41d01ff6e0db5326677c
Bishoy Boules <79402748+BishoyBoules@users.noreply.github.com>
2022-10-25 22:00:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,48 +2,39 @@ let result = 0
 let display = ''
 let operator = false
 let firstNumber = 0
-let operatorDisplay = '='
+let operatorDisplay = ''
 let dotClicked = false
 
-function clear () {
-    operator = false
-    dotClicked = false
-    display = ''
-    result = 0
-    firstNumber = 0
-    operatorDisplay = ''
-    resultElement.value = display
+function clear() {
+  operator = false
+  dotClicked = false
+  display = ''
+  result = 0
+  firstNumber = 0
+  operatorDisplay = ''
+  resultElement.value = display
 }
 
-const add = (x, y) => {
-    let theResult = x+y
-    return theResult.toFixed(5)
-}
+const add = (x, y) => x + y
 
-const subtract = (x, y) => {
-    let theResult = x-y
-    return theResult.toFixed(5)
-}
+const subtract = (x, y) => x - y
 
-const multiply = (x, y) =>{
-    let theResult = x*y
-    return theResult.toFixed(5)
-}
+const multiply = (x, y) => x * y
 
 const divide = (x, y) => {
-    if(y === 0){
-        clear()
-        alert("you can't divide by zero")
-        return ''
-    }
-    let theResult = x/y
-    return theResult.toFixed(5)
+  if (y === 0) {
+    clear()
+    alert("you can't divide by zero")
+    return ''
+  }
+  let theResult = x / y
+  if (theResult % 1) {
+    return parseFloat(theResult.toFixed(3))
+  } else {
+    return theResult
+  }
 }
 
-const divideElement = document.getElementById('divide')
-const multiElement = document.getElementById('divide')
-const addElement = document.getElementById('add')
-const subElement = document.getElementById('subtract')
 const clearElement = document.getElementById('clear')
 const resultElement = document.getElementById('result')
 
@@ -54,61 +45,50 @@ clearElement.addEventListener('click', clear)
 resultElement.value = display
 
 const operate = (element) => {
-    if(element.classList.contains('number')){
-        if(dotClicked){
-            display += element.textContent
-        } else {
-            display = element.textContent
-        }
-        if(operator){
-            result = parseFloat(display)
-        }
-        else {
-            firstNumber = parseFloat(display)
-        }
-    } else if(element.classList.contains('operator')){
-        dotClicked = false
-        if(operator){
-            result = parseFloat(display)
-            if(operatorDisplay === '+'){
-                display = add(firstNumber, result)
-            } else if (operatorDisplay === '-'){
-                display = subtract(firstNumber, result)
-            } else if(operatorDisplay === 'x'){
-                display = multiply(firstNumber, result)
-            } else if(operatorDisplay === '/'){
-                display = divide(firstNumber, result)
-            } else if(operatorDisplay === '%'){
-                display = divide(firstNumber, 100)
-            }
-            firstNumber = parseFloat(display)
-        } else {
-            firstNumber = parseFloat(display)
-        }
-        operatorDisplay = element.textContent
-        operator = true
-    }   else if(element.classList.contains('dot')) {
-        if(dotClicked){
-            alert("you can't enter more than one dot")
-        } else {
-            dotClicked = true
-            display += element.textContent
-        }
-    }   else if(element.classList.contains('equal')){
-        operator = false
-        dotClicked = false
-        if(operatorDisplay === '+'){
-            display = add(firstNumber, result)
-        } else if (operatorDisplay === '-'){
-            display = subtract(firstNumber, result)
-        } else if(operatorDisplay === 'x'){
-            display = multiply(firstNumber, result)
-        } else if(operatorDisplay === '/'){
-            display = divide(firstNumber, result)
-        } else if(operatorDisplay === '%'){
-            display = divide(firstNumber, 100)
-        }
-        firstNumber = parseFloat(display)
-    } 
-        resultElement.value = display
+  if (element.classList.contains('number')) {
+    if (operator) {
+      display += element.textContent
+      result = parseFloat(display)
+      switch (operatorDisplay) {
+        case '+':
+          display = add(firstNumber, result)
+          break
+        case '-':
+          display = subtract(firstNumber, result)
+          break
+        case 'x':
+          display = multiply(firstNumber, result)
+          break
+        case '/':
+          display = divide(firstNumber, result)
+          break
+        case '%':
+          display = divide(firstNumber, 100)
+          break
+        default:
+          display = ''
+      }
+      firstNumber = display
+    } else {
+      display += element.textContent
+      firstNumber = parseFloat(display)
+    }
+  } else if (element.classList.contains('operator')) {
+    dotClicked = false
+    display = ''
+    operatorDisplay = element.textContent
+    operator = true
+  } else if (element.classList.contains('dot')) {
+    if (dotClicked) {
+      alert("you can't enter more than one dot")
+    } else {
+      dotClicked = true
+      display += element.textContent
+    }
+  } else if (element.classList.contains('equal')) {
+    operator = false
+    dotClicked = false
+    firstNumber = display
+  }
+  resultElement.value = display
 }
