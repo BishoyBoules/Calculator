36934614818ad2be6b66f83302b99ec03cccfbd7
Bishoy Boules <79402748+BishoyBoules@users.noreply.github.com>
2022-10-26 10:02:42 +0200

    last commit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let result = 0
 let display = ''
+let secDisplay = ''
 let operator = false
 let firstNumber = 0
 let operatorDisplay = ''
@@ -9,6 +10,7 @@ function clear() {
   operator = false
   dotClicked = false
   display = ''
+  secDisplay = ''
   result = 0
   firstNumber = 0
   operatorDisplay = ''
@@ -47,8 +49,24 @@ resultElement.value = display
 const operate = (element) => {
   if (element.classList.contains('number')) {
     if (operator) {
+      if(display){
+        firstNumber = display
+      }
+      display = ''
+      secDisplay += element.textContent
+      result = parseFloat(secDisplay)
+      resultElement.value = secDisplay
+    } else {
       display += element.textContent
-      result = parseFloat(display)
+      firstNumber = parseFloat(display)
+      resultElement.value = display
+    }
+  } else if (element.classList.contains('operator')) {
+    dotClicked = false
+    display = ''
+    secDisplay = ''
+    operator = true
+    if(result){
       switch (operatorDisplay) {
         case '+':
           display = add(firstNumber, result)
@@ -62,32 +80,51 @@ const operate = (element) => {
         case '/':
           display = divide(firstNumber, result)
           break
-        case '%':
-          display = divide(firstNumber, 100)
-          break
-        default:
-          display = ''
       }
-      firstNumber = display
-    } else {
-      display += element.textContent
-      firstNumber = parseFloat(display)
+      resultElement.value = display
     }
-  } else if (element.classList.contains('operator')) {
-    dotClicked = false
-    display = ''
     operatorDisplay = element.textContent
-    operator = true
   } else if (element.classList.contains('dot')) {
     if (dotClicked) {
       alert("you can't enter more than one dot")
     } else {
       dotClicked = true
-      display += element.textContent
+      if(operator){
+        secDisplay +=  element.textContent
+        resultElement.value = secDisplay
+      } else {
+        display += element.textContent
+        resultElement.value = display
+      }
     }
   } else if (element.classList.contains('equal')) {
     operator = false
     dotClicked = false
+    switch (operatorDisplay) {
+      case '+':
+        display = add(firstNumber, result)
+        break
+      case '-':
+        display = subtract(firstNumber, result)
+        break
+      case 'x':
+        display = multiply(firstNumber, result)
+        break
+      case '/':
+        display = divide(firstNumber, result)
+        break
+    }
+    firstNumber = display
+    resultElement.value = display.toFixed(3)
+  } else if (element.classList.contains('pcg')) {
+    display = firstNumber / 100
+    firstNumber = parseFloat(display)
+    resultElement.value = display
+    operator = true
+  } else if (element.classList.contains('sign')) {
+    display = firstNumber * -1
+    firstNumber = parseFloat(display)
+    resultElement.value = display
+    operator = true
   }
-  resultElement.value = display
 }
